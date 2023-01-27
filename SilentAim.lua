b33b9fde9d8f19de947b808f3bdf678698fdfd62
Von <112925544+appealaccepted@users.noreply.github.com>
2023-01-27 17:21:04 -0500

    Update SilentAim.lua

--- SilentAim.lua
+++ SilentAim.lua
@@ -80,7 +80,7 @@ oldIndex = hookmetamethod(game, "__index", function(self, Index)
             end
         end
 
-        if Targete ~= nil and Targete[Options.Upper] and Targete:FindFirstChild("Humanoid").Health > 0 then
+        if Targete ~= nil and Targete[Options.Head] and Targete:FindFirstChild("Humanoid").Health > 0 then
             if SilentAim then
                 local ShootThis = Targete[Options.Torso] or Targete[Options.Head] or Targete[Options.Upper]
                 local Predicted_Position = ShootThis.CFrame + (ShootThis.Velocity * _G.Prediction + Vector3.new(0,-0,0)) --  (-1) = Less blatant
