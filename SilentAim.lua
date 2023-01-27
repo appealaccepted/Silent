ed4cedbcb358adc2e0ed1dbc08e57b31e8e534c4
Von <112925544+appealaccepted@users.noreply.github.com>
2023-01-26 21:50:48 -0500

    Update SilentAim.lua

--- SilentAim.lua
+++ SilentAim.lua
@@ -4,42 +4,6 @@ _G.Prediction =  (  .16  )
 
 _G.AimKey =  (  "z"  )
 
---game:GetService("RunService").Heartbeat:Connect(function()
-  -- pcall(function()
-      -- for i,v in pairs(game.Players:GetChildren()) do
-        --   if v.Name ~= game.Players.LocalPlayer.Name then
-         --      local hrp = v.Character.HumanoidRootPart
-        --       hrp.Velocity = Vector3.new(hrp.Velocity.X, 0, hrp.Velocity.Z)    
-        --       hrp.AssemblyLinearVelocity = Vector3.new(hrp.Velocity.X, 0, hrp.Velocity.Z)  
-        --   end
-    --   end
- --  end)
---end)
-   
---game:GetService("RunService").Heartbeat:Connect(function()
- --  pcall(function()
-   --    for i,v in pairs(game.Players:GetChildren()) do
-    --       if v.Name ~= game.Players.LocalPlayer.Name then
-     --          local Head = v.Character.Head
-     --          hrp.Velocity = Vector3.new(hrp.Velocity.X, 0, hrp.Velocity.Z)    
-     --          hrp.AssemblyLinearVelocity = Vector3.new(hrp.Velocity.X, 0, hrp.Velocity.Z)  
-        --   end
-    --   end
- --  end)
---end)
-   
---game:GetService("RunService").Heartbeat:Connect(function()
-  -- pcall(function()
-     --  for i,v in pairs(game.Players:GetChildren()) do
-         --  if v.Name ~= game.Players.LocalPlayer.Name then
-          --     local Upper = v.Character.UpperTorso
-           --    hrp.Velocity = Vector3.new(hrp.Velocity.X, 0, hrp.Velocity.Z)    
-          --     hrp.AssemblyLinearVelocity = Vector3.new(hrp.Velocity.X, 0, hrp.Velocity.Z)  
-          -- end
-      -- end
- --  end)
---end)
-
 local SilentAim = true
 local LocalPlayer = game:GetService("Players").LocalPlayer
 local Players = game:GetService("Players")
